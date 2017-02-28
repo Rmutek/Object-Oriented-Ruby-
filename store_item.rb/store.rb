8b0c36c36fa2efc39f3d52fab796d57f07cf4144
Rebecca Mutek <mutek.b@gmail.com>
2017-02-28 17:52:54 -0600

    added discount

--- store_item.rb/store.rb
+++ store_item.rb/store.rb
@@ -10,7 +10,7 @@ class Orders
   attr_reader :flavor, :container
   attr_writer :topping, :price
 
-  def initalize(flavor, container, topping, price) 
+  def initialize(flavor, container, topping, price) 
     @flavor = flavor
     @container = container
     @topping = topping
@@ -30,8 +30,49 @@ class Orders
   end
 end 
 
-order1 = Order.new("Vanilla", "Cone", "sprinkles", "4")
-order2 = Order.new("Swirl", "Cup", "Nuts", "3")
+order1 = Orders.new("Vanilla", "Cone", "sprinkles", "4")
+order2 = Orders.new("Swirl", "Cup", "Nuts", "3")
 
 order2.print_info
 
+class Orders
+  attr_reader :flavor, :container
+  attr_writer :topping, :price
+
+  def initialize(input_options) 
+    @flavor = input_options [:flavor]
+    @container = input_options [:container]
+    @topping = input_options [:topping]
+    @price = input_options [:price]
+  end
+
+  def print_info 
+    puts "#{@flavor} ice cream in a #{@container}"
+  end
+
+  def give_discount
+    @price = @price * 0.95
+  end
+
+  def price=(price)
+    @price = price 
+  end
+end 
+
+order1 = Orders.new({flavor: "Vanilla", container: "Cone", topping: "sprinkles", price: "4"})
+order2 = Orders.new({:flavor => "Swirl", :container => "Cup", :topping => "Nuts", :price => "3"})
+
+order2.print_info
+order1.give_discount
+
+
+
+def calculate_sum(array_of_numbers)
+  sum = 0
+  array_of_numbers.each do |number|
+    sum += number
+  end
+  return sum
+end
+
+p calculate_sum([3, 4, 2])
