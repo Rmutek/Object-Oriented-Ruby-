76b684b2b05c4465e9283614dd582f1a6c8efdfd
Rebecca Mutek <mutek.b@gmail.com>
2017-02-27 20:40:08 -0600

    hash added

---
+++ store_item.rb/store.rb
@@ -0,0 +1,7 @@
+ice_cream1 = {:flavor => "Vanilla", :cone => "cone", :topping => "sprinkles", :price => 4}
+ice_cream2 = {:flavor => "Swirl", :cone => "cone", :topping => "gummie_bears", :price => 5}
+ice_cream3 = {:flavor => "Mint", :cone => "cup", :topping => "whipped_cream", :price => 3}
+
+
+puts "#{ice_cream1[:flavor]} ice cream in a #{ice_cream1[:cone]} with #{ice_cream1[:topping]} is $ #{ice_cream1[:price]}"
+puts "#{ice_cream1[:flavor]} ice cream in a #{ice_cream1[:cone]} with #{ice_cream1[:topping]} is $ #{ice_cream1[:price]}"
